7a034f4ce776f81c6974dff6049d408ea082608c
Jaynarol <jaynarol@gmail.com>
2023-09-27 18:11:20 +0700

    fix: image path relative for ghpage

--- src/components/Creations.tsx
+++ src/components/Creations.tsx
@@ -4,43 +4,43 @@ import Link from 'next/link'
 const creations = [
   {
     text: 'DEEP EARTH',
-    imageMobile: '/images/mobile/image-deep-earth.jpg',
-    imageDesktop: '/images/desktop/image-deep-earth.jpg',
+    imageMobile: './images/mobile/image-deep-earth.jpg',
+    imageDesktop: './images/desktop/image-deep-earth.jpg',
   },
   {
     text: 'NIGHT ARCADE',
-    imageMobile: '/images/mobile/image-night-arcade.jpg',
-    imageDesktop: '/images/desktop/image-night-arcade.jpg',
+    imageMobile: './images/mobile/image-night-arcade.jpg',
+    imageDesktop: './images/desktop/image-night-arcade.jpg',
   },
   {
     text: 'SOCCER TEAM VR',
-    imageMobile: '/images/mobile/image-soccer-team.jpg',
-    imageDesktop: '/images/desktop/image-soccer-team.jpg',
+    imageMobile: './images/mobile/image-soccer-team.jpg',
+    imageDesktop: './images/desktop/image-soccer-team.jpg',
   },
   {
     text: 'THE GRID',
-    imageMobile: '/images/mobile/image-grid.jpg',
-    imageDesktop: '/images/desktop/image-grid.jpg',
+    imageMobile: './images/mobile/image-grid.jpg',
+    imageDesktop: './images/desktop/image-grid.jpg',
   },
   {
     text: 'FROM UP ABOVE VR',
-    imageMobile: '/images/mobile/image-from-above.jpg',
-    imageDesktop: '/images/desktop/image-from-above.jpg',
+    imageMobile: './images/mobile/image-from-above.jpg',
+    imageDesktop: './images/desktop/image-from-above.jpg',
   },
   {
     text: 'POCKET BOREALIS',
-    imageMobile: '/images/mobile/image-pocket-borealis.jpg',
-    imageDesktop: '/images/desktop/image-pocket-borealis.jpg',
+    imageMobile: './images/mobile/image-pocket-borealis.jpg',
+    imageDesktop: './images/desktop/image-pocket-borealis.jpg',
   },
   {
     text: 'THE CURIOSITY',
-    imageMobile: '/images/mobile/image-curiosity.jpg',
-    imageDesktop: '/images/desktop/image-curiosity.jpg',
+    imageMobile: './images/mobile/image-curiosity.jpg',
+    imageDesktop: './images/desktop/image-curiosity.jpg',
   },
   {
     text: 'MAKE IT FISHEYE',
-    imageMobile: '/images/mobile/image-fisheye.jpg',
-    imageDesktop: '/images/desktop/image-fisheye.jpg',
+    imageMobile: './images/mobile/image-fisheye.jpg',
+    imageDesktop: './images/desktop/image-fisheye.jpg',
   },
 ]
 
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 const Features = () => (
   <section className="mb-14 mt-28 flex flex-col items-center space-y-10 text-center sm:justify-start lg:flex-row lg:items-end lg:px-44 lg:text-left">
-    <Image className="lg:hidden" width={654} height={448} src="/images/mobile/image-interactive.jpg" alt="interactive" />
-    <Image className="hidden h-full object-cover lg:block lg:w-3/5" width={731} height={500} src="/images/desktop/image-interactive.jpg" alt="interactive" />
+    <Image className="lg:hidden" width={654} height={448} src="./images/mobile/image-interactive.jpg" alt="interactive" />
+    <Image className="hidden h-full object-cover lg:block lg:w-3/5" width={731} height={500} src="./images/desktop/image-interactive.jpg" alt="interactive" />
     <div className="space-y-5 bg-primary px-5 md:p-10 lg:ml-[-10%] lg:w-1/2 lg:pb-0 xl:p-20">
       <h2 className="font-secondary text-4xl lg:text-5xl">THE LEADER IN INTERACTIVE VR</h2>
       <p className="text-secondary">
